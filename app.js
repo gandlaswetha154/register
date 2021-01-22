38131ad2efd4940115efa626009883ff1e3efefe
gandlaswetha154 <77266711+gandlaswetha154@users.noreply.github.com>
2021-01-22 19:02:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express=require("express");
+const path=require("path");
+const mysql=require("mysql");
+const dotenv=require("dotenv");
+
+dotenv.config({path: './.env'});
+
+const app=express();
+const db=mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+});
+const publicDirectory=path.join(__dirname,'./login');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({ extended:false}));
+app.use(express.json());
+app.set('view engine','hbs');
+
+db.connect((error)=>{
+  if(error){
+    console.log(error)
+  }else{
+    console.log("MYSQL Connected");
+  }
+});
+
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+
+app.listen(5000,(req,res)=>{
+  console.log("server started on port 5000");
+});
